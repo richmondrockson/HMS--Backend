3e31fe0820565c4a358e686bdf706dd6d4b4a625
richmondrockson <richmond.rockson@amalitech.org>
2025-10-20 23:12:59 +0000

    Initial backend setup with express and MySQL connection

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("HMS Backend API is running...");
+});
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
